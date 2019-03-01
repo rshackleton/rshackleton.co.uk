78e5e7e56403eb0592e8ed3fd5fa32679df96a40
Unknown <richardshackleton@netconstruct.co.uk>
2019-03-01 19:12:59 +0000

    Add source urls to code blocks, closes #34

--- src/components/shared/SyntaxHighlightedCodeBlock.jsx
+++ src/components/shared/SyntaxHighlightedCodeBlock.jsx
@@ -2,17 +2,40 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { atomDark as style } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-import { SyntaxHighlighter } from './SyntaxHighlightedCodeBlock.styles';
+import {
+  Caption,
+  CaptionLink,
+  Container,
+  SyntaxHighlighter,
+} from './SyntaxHighlightedCodeBlock.styles';
 
-const SyntaxHighlightedCodeBlock = ({ code, language }) => (
-  <SyntaxHighlighter language={language} style={style}>
-    {code}
-  </SyntaxHighlighter>
+const SyntaxHighlightedCodeBlock = ({ code, language, sourceUrl }) => (
+  <Container>
+    <SyntaxHighlighter
+      hasCaption={sourceUrl && sourceUrl.length}
+      language={language}
+      style={style}
+    >
+      {code}
+    </SyntaxHighlighter>
+    {sourceUrl && sourceUrl.length ? (
+      <Caption>
+        <CaptionLink
+          href={sourceUrl}
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+        >
+          View Source
+        </CaptionLink>
+      </Caption>
+    ) : null}
+  </Container>
 );
 
 SyntaxHighlightedCodeBlock.propTypes = {
   code: PropTypes.string.isRequired,
   language: PropTypes.string.isRequired,
+  sourceUrl: PropTypes.string.isRequired,
 };
 
 export default SyntaxHighlightedCodeBlock;
