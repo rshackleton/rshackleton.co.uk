e310609996004e9962311296b9a2fe84a047e4f5
Richard Shackleton <richard.shackleton@idhl.co.uk>
2020-09-06 12:16:39 +0100

    Add preview api endpoints

---
+++ src/pages/api/preview.ts
@@ -0,0 +1,78 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { getArticle, getArticleListing, getContentPage, getHomePage } from '@/lib/api';
+
+export default async function preview(req: NextApiRequest, res: NextApiResponse) {
+  // Check the secret and next parameters
+  // This secret should only be known to this API route and the CMS
+  if (
+    req.query.secret !== process.env.KONTENT_PREVIEW_SECRET ||
+    typeof req.query.slug !== 'string' ||
+    typeof req.query.type !== 'string'
+  ) {
+    return res
+      .status(401)
+      .json({ message: 'Invalid token or slug or type parameter not specified' });
+  }
+
+  // Fetch the headless CMS to check if the provided `slug` exists
+  const slug = req.query.slug;
+  const type = req.query.type;
+
+  switch (type) {
+    case 'article': {
+      const itemResponse = await getArticle(slug, true);
+
+      if (!itemResponse.firstItem) {
+        return res.status(401).json({ message: 'Item not found' });
+      }
+
+      enablePreviewAndRedirectTo(`/articles/${itemResponse.firstItem.slug.value}`);
+      break;
+    }
+
+    case 'article_listing': {
+      const itemResponse = await getArticleListing(true);
+
+      if (!itemResponse.firstItem) {
+        return res.status(401).json({ message: 'Item not found' });
+      }
+
+      enablePreviewAndRedirectTo(`/articles/${itemResponse.firstItem.slug.value}`);
+      break;
+    }
+
+    case 'content_page': {
+      const itemResponse = await getContentPage(slug, true);
+
+      if (!itemResponse.firstItem) {
+        return res.status(401).json({ message: 'Item not found' });
+      }
+
+      enablePreviewAndRedirectTo(`/articles/${itemResponse.firstItem.slug.value}`);
+      break;
+    }
+
+    case 'homepage': {
+      const itemResponse = await getHomePage(true);
+
+      if (!itemResponse.firstItem) {
+        return res.status(401).json({ message: 'Item not found' });
+      }
+
+      enablePreviewAndRedirectTo(`/articles/${itemResponse.firstItem.slug.value}`);
+      break;
+    }
+
+    // If a content item doesn't exist prevent preview mode from being enabled
+    default: {
+      return res.status(401).json({ message: 'Invalid type' });
+    }
+  }
+
+  function enablePreviewAndRedirectTo(url: string) {
+    res.setPreviewData({});
+    res.writeHead(307, { Location: url });
+    res.end();
+  }
+}
