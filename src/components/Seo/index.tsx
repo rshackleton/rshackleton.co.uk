89372665549dd6ca90be31c88f18172484d525e5
Richard Shackleton <richard.shackleton@idhl.co.uk>
2020-10-10 15:59:35 +0100

    Add canonical tag

--- src/components/Seo/index.tsx
+++ src/components/Seo/index.tsx
@@ -7,6 +7,7 @@ const Seo: React.FC<ISeoProps> = ({ canonicalUrl, description, image, keywords, 
   return (
     <Head>
       <title>{`${title} | ${process.env.NEXT_PUBLIC_SITE_TITLE}`}</title>
+      <link rel="canonical" href={`https://${process.env.NEXT_PUBLIC_HOST}/${canonicalUrl}`} />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta property="og:title" content={`${title} | ${process.env.NEXT_PUBLIC_SITE_TITLE}`} />
